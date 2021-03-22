271e65cd29cc8fa0e212bfc3aa990d4fe50c6766
unknown <kevin.lin@nyu.edu>
2021-03-22 16:05:52 +0100

    Syncing up directory

--- sampleBlogProject/settings.py
+++ sampleBlogProject/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '4ib&n)6qr^n4ztd8ehuy=%5s4dczzb)ushaee4^a_2yuy=me0z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -128,3 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'klin1996@gmail.com'
+EMAIL_HOST_PASSWORD = 'zrqncvyhrynrqqae'
